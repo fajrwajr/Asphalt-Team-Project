58c74bbfc8add966caf55c7ffb0dc6e85af21356
55746 <Lawhead.gavin@live.jobcorps.org>
2022-08-07 00:00:12 +0000

    ALMOST

--- src/front/js/pages/stripingandsignage.js
+++ src/front/js/pages/stripingandsignage.js
@@ -1,59 +1,60 @@
-// import React from "react";
-// import image5 from "../../img/cracksealing2.jpg";
+import React from "react";
+import image5 from "../../img/cracksealing2.jpg";
 
-// export const Stripingandsignage = () => {
-//   return (
-//     <div>
-//       <img src={image5} className="servicepagesimg" />
-
-//       <p className="text-warning">
-//         <h1>Hot Pour Crack Filling</h1>
-//         Crack sealing your parking area on a yearly basis is the most cost
-//         effective way to reduce costly asphalt repairs. Nashville Sealing &
-//         Striping uses only the highest grade hot rubberized crack sealing
-//         material exceeding federal specifications. Our material will not track
-//         in any situation.
-//         <br></br>• Prevents water intrusion - The number one cause of asphalt
-//         failure.
-//         <br></br>• Most cost effective way to protect your asphalt investment.
-//         <br></br>• This highly elastic material remains flexible to temperatures
-//         of 20 below zero. illing page
-//         <br></br>
-//       </p>
-//     </div>
-//   );
-// };
-
-
-// <</h1>h1>Striping & Sig</h1>nage<h1>
-//  Missouri Asphalt Sealing & Striping offers complete layout and line striping services using the best striping equipment and quality paint available. Our superior service is well known by general contractors as well as municipal and commercial building owners. We offer competitive quotes and work to complete the job promptly.
-
-// We will execute your design layout, inspect and install all necessary line striping to ensure your compliance with the Americans with Disabilities Act (ADA).
-
-// STRIPING SERVICES
-// • ADA Compliance
-// • Striping Removal
-// • Driver Training Course
-// • Directional Thermoplastic & 3M Stencils
-// • Light Pole Bases
-// • Crosswalks / Access
-// • Stenciling
-// • Commercial / Municipal Striping
-// • Handicapped
-// • Fire Lane
-// • No Parking
-// • Indoor Warehouse Striping
-// • Running Tracks
-// • Airports
-
-// SAFETY
-// Professional Line Striping assists your visitors in the safe use of your parking lot. It directs traffic to the proper route of entrance and exit, allowable areas to park, loading zones, crosswalks, and defines drive aisles that are wide enough to avoid accidents with cars and pedestrians. Properly designed parking spaces also maximize the number of cars you can safely accommodate.
-
-// UPGRADE PROPERTY APPEARANCE
-// Customers appreciate a well designed and well marked parking system. The parking lot is the first thing that customers or visitors see. A freshly painted parking lot can lend an attractive impression to the overall image of your property.
-
-// QUALITY
-// At Missouri Asphalt Sealing & Striping, we expertly apply line striping so that it results in consistent, sharp lines with well defined edges, starts and stops.
-
-// SIGNAGE
-// Missouri Asphalt Sealing & Striping offers services in Signage installation, Bollard Installation, Concrete Bumper Block installation, etc. 
+export const Stripingandsignage = () => {
+  return (
+    <div>
+      <img src={image5} className="servicepagesimg" />
+      <h1>Striping & Signage</h1>
+      <bold>Nashville Asphalt Sealing & Striping</bold> offers complete layout
+      and line striping services using the best striping equipment and quality
+      paint available. Our superior service is well known by commercial
+      contractors. We offer competitive quotes and work to complete the job
+      promptly.
+      <br></br>
+      We will execute your design layout, inspect and install all necessary line
+      striping to ensure your compliance with the Americans with Disabilities
+      Act (ADA).
+      <br></br>
+      <h3>STRIPING SERVICES</h3>• ADA Compliance
+      <br></br>• Striping Removal
+      <br></br>• Driver Training Course
+      <br></br>• Directional Thermoplastic & 3M Stencils
+      <br></br>• Light Pole Bases
+      <br></br>• Crosswalks / Access
+      <br></br>• Stenciling
+      <br></br>• Commercial / Municipal Striping
+      <br></br>• Handicapped
+      <br></br>• Fire Lane
+      <br></br>• No Parking
+      <br></br>• Indoor Warehouse Striping
+      <br></br>• Running Tracks
+      <br></br>• Airports
+      <br></br>
+      <h3>SAFETY</h3>
+      Professional Line Striping assists your visitors in the safe use of your
+      parking lot. It directs traffic to the proper route of entrance and exit,
+      allowable areas to park, loading zones, crosswalks, and defines drive
+      aisles that are wide enough to avoid accidents with cars and pedestrians.
+      Properly designed parking spaces also maximize the number of cars you can
+      safely accommodate.
+      <br></br>
+      <h3>UPGRADE PROPERTY APPEARANCE</h3>
+      Customers appreciate a well designed and well marked parking system. The
+      parking lot is the first thing that customers or visitors see. A freshly
+      painted parking lot can lend an attractive impression to the overall image
+      of your property.
+      <br></br>
+      <h3>QUALITY</h3>
+      At Missouri Asphalt Sealing & Striping, we expertly apply line striping so
+      that it results in consistent, sharp lines with well defined edges, starts
+      and stops.
+      <br></br>
+      <h3>SIGNAGE</h3>
+      Missouri Asphalt Sealing & Striping offers services in Signage
+      installation, Bollard Installation, Concrete Bumper Block installation,
+      etc.
+      <br></br>
+    </div>
+  );
+};
